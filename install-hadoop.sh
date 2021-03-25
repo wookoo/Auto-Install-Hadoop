0776d7a5e96f850ed02ab351f4716526b5b7c33b
wookoo <nanayagoon@daum.net>
2021-03-25 19:24:41 +0900

    test

---
+++ install-hadoop.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/wookoo/Auto-Install-Hadoop.git
+wget https://archive.apache.org/dist/hadoop/core/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+tar -xvzf hadoop-3.3.0.tar.gz
+rm hadoop-3.3.0.tar.gz
+sudo apt-get install openjdk-8-jdk -y
