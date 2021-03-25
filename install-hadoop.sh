2a5b915ec6bd487ca84db49ada64e904dc2f972a
wookoo <nanayagoon@daum.net>
2021-03-25 19:41:18 +0900

    스크립트 수정중

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/wookoo/Auto-Install-Hadoop.git
-wget https://archive.apache.org/dist/hadoop/core/hadoop-3.3.0/hadoop-3.3.0.tar.gz
-tar -xvzf hadoop-3.3.0.tar.gz
-rm hadoop-3.3.0.tar.gz
+curl -O https://archive.apache.org/dist/hadoop/core/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+mv hadoop-3.3.0.tar.gz $HOME/
+tar -xvzf $HOME/hadoop-3.3.0.tar.gz
+rm $HOME/hadoop-3.3.0.tar.gz
 sudo apt-get install openjdk-8-jdk -y
